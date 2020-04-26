a08cd12f728a9b9b62ae2b12c2465a91c1b4052b
HannahDeWachter <r0738008@student.thomasmore.be>
2020-04-26 15:15:09 +0200

    get stats from database

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -8,3 +8,29 @@ primus = Primus.connect("http://localhost:3000", {
         , retries: 10 // Number: How many times we should try to reconnect.
     }
 });
+
+let parent = document.querySelector(".countries");
+
+let showNumbers = (json) => {
+    json.data.forEach(stat => {
+        let child = document.createElement('div');
+        let country = document.createElement('h1');
+        let number = document.createElement('p');
+        country.innerHTML = stat.country;
+        number.innerHTML = stat.number;
+        child.append(country);
+        child.append(number);
+        parent.append(child);
+    });
+}
+
+fetch('http://localhost:3000/api/v1/corona', {
+    method: "get",
+    headers: {
+        'Content-Type': 'application/json'
+    },
+}).then(response => {
+    return response.json();
+}).then(json => {
+    showNumbers(json);
+});
