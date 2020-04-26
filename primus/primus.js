acdea2e7ca4bccf4f2b212c5a2ca218e1e7cdf5f
HannahDeWachter <r0738008@student.thomasmore.be>
2020-04-26 17:49:01 +0200

    primus from videos

--- primus/primus.js
+++ primus/primus.js
@@ -4,6 +4,11 @@ let go = (server) => {
     let primus = new Primus(server, {/* options */ });
     primus.on('connection', (spark) => {
         console.log("Received spark");
+
+        spark.on('data', (data) => {
+            console.log(data);
+            primus.write(data);
+        });
     });
 }
 
